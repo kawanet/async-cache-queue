d47cee690192d14e1ccd811fe5ad52adf187d18c
Yusuke Kawasaki <u-suke@kawa.net>
2020-12-28 11:22:00 +0900

    check item expired and key exists

--- lib/timed-storage.ts
+++ lib/timed-storage.ts
@@ -29,20 +29,51 @@ export class TimedStorage<I extends IItem> extends DataStorage<TimedItem> implem
         while (keys.length) {
             const first = keys[0];
             const item = store[first];
-            if (now < item?.ttl) break;
+            if (now <= item?.ttl) break;
             keys.shift();
             delete store[first];
         }
 
         // cached item
-        return store[key] as I;
+        const item = store[key];
+
+        // check the item expired
+        // this would rarely happen though
+        if (now > item?.ttl) {
+            removeKey(keys, key);
+            delete store[key];
+            return;
+        }
+
+        return item as I;
     }
 
     set(key: string, item: I): void {
         const {keys, expires} = this;
         const store = this.store();
+
+        // check the key already exists
+        // this would happen on refreshing
+        if (store[key]) {
+            removeKey(keys, key);
+        }
+
         (item as TimedItem).ttl = Date.now() + expires;
         store[key] = item;
         keys.push(key);
+    }
+}
+
+/**
+ * sadly O(n)
+ */
+
+function removeKey(keys: string[], key: string) {
+    const length = keys.length;
+    for (let i = 0; i < length; i++) {
+        if (keys[i] === key) {
+            keys.splice(i, 1);
+            break;
+        }
     }
 }
