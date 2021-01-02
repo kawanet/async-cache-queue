a22c094b4b459fa992e957184ef5bebc15a260a7
Yusuke Kawasaki <u-suke@kawa.net>
2021-01-03 00:46:29 +0900

    async-cache-queue.d.ts

--- types/async-cache-queue.d.ts
+++ types/async-cache-queue.d.ts
@@ -1,6 +1,7 @@
 /**
- * async-cache-queue.ts
+ * async-cache-queue.d.ts
  */
+
 export interface QueueOptions<IN = any, OUT = any> {
     /**
      * Set a cache TTL in milliseconds since a succeeded result resolved.
@@ -10,7 +11,9 @@ export interface QueueOptions<IN = any, OUT = any> {
      *
      * @default `0` to disable the cache.
      */
+
     cache?: number;
+
     /**
      * Set a negative cache TTL in milliseconds since a failed result rejected.
      * The internal cache feature stores its value as a Promise but not rejected reasons.
@@ -19,7 +22,9 @@ export interface QueueOptions<IN = any, OUT = any> {
      *
      * @default `0` to disable the negative cache.
      */
+
     negativeCache?: number;
+
     /**
      * Set a refreshing interval in milliseconds.
      * It executes the function to fetch a new result in background for the next coming request.
@@ -27,20 +32,26 @@ export interface QueueOptions<IN = any, OUT = any> {
      *
      * @default `0` to disables the feature.
      */
+
     refresh?: number;
+
     /**
      * Set a maximum number of items stored in the internal cache.
      * Note that the restriction is applied only for the internal cache but not for external `storage`.
      *
      * @default `0` to disables the restriction.
      */
+
     maxItems?: number;
+
     /**
      * Set a function to stringify cache key.
      *
      * @default: `(arg) => JSON.stringify(arg)`
      */
+
     hasher?: (arg: any) => string;
+
     /**
      * Set an external key-value storage adapter which has `.get(key)` and `.set(key, val)` methods.
      * Note that the `cache` option above does not affect for the external storage.
@@ -48,20 +59,26 @@ export interface QueueOptions<IN = any, OUT = any> {
      *
      * @default `undefined` not to connect to any external storage.
      */
+
     storage?: KVS<OUT>;
+
     /**
      * Set a maximum limit number of concurrent working jobs.
      * The rest jobs will wait to start until another slot opened.
      *
      * @default `0` to disable the throttle feature.
      */
+
     concurrency?: number;
+
     /**
      * Set time in milliseconds to stop a long working unresolved job since it started.
      *
      * @default `0` to disable the timeout feature.
      */
+
     timeout?: number;
+
     /**
      * Set a function to resolve yet another fallback results,
      * instead of long working jobs which has exceeded `timeout` milliseconds.
@@ -69,8 +86,10 @@ export interface QueueOptions<IN = any, OUT = any> {
      *
      * @default `() => Promise.reject(new Error("timeout: ${timeout}ms"));`
      */
+
     timeoutFallback?: (arg?: IN) => OUT;
 }
+
 /**
  * Interface for external key-value storage, e.g. https://www.npmjs.com/package/keyv
  *
@@ -78,15 +97,21 @@ export interface QueueOptions<IN = any, OUT = any> {
  * Note that `null` and `undefined` value could not be cached via the external storage.
  * Use another value instead for the cases.
  */
+
 export interface KVS<T = any> {
-    get(key: string): Promise<T>;
-    set(key: string, value: T): Promise<void>;
+    get(key: string): T | Promise<T>;
+
+    set(key: string, value: T): void | Promise<void> | this;
 }
+
 /**
  * It returns a function to create a queue manager.
  */
+
 export declare function queueFactory(options?: QueueOptions): (<IN, OUT>(fn: ((arg?: IN) => Promise<OUT>)) => ((arg?: IN) => Promise<OUT>));
+
 /**
  * It purges all cache storage handled in the library.
  */
+
 export declare function clearCache(): void;
