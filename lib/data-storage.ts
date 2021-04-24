c2d7b9c83dd49490a82ef90f720db11683964282
Yusuke Kawasaki <u-suke@kawa.net>
2021-04-24 22:01:29 +0900

    interface EnvelopeKVS<T>

--- lib/data-storage.ts
+++ lib/data-storage.ts
@@ -1,32 +1,22 @@
 /**
  * data-storage.ts
  */
-import {ACQ} from "../types/async-cache-queue";
 
 export interface Envelope<T> {
     value: T;
 }
 
-export abstract class EnvelopeKVS<T> implements ACQ.MapLike<T> {
-    get(key: string): T {
-        const item = this.getItem(key);
-        if (item) return item.value;
-    }
+export interface EnvelopeKVS<T> {
+    getItem(key: string): Envelope<T>;
 
-    set(key: string, value: T): void {
-        this.setItem(key, {value: value});
-    }
-
-    abstract getItem(key: string): Envelope<T>;
-
-    abstract setItem(key: string, value: Envelope<T>): void;
+    setItem(key: string, value: Envelope<T>): void;
 }
 
 /**
  * Persistent Storage
  */
 
-export class SimpleStorage<T> extends EnvelopeKVS<T> {
+export class SimpleStorage<T> implements EnvelopeKVS<T> {
     private items = {} as { [key: string]: Envelope<T> };
 
     getItem(key: string): Envelope<T> {
